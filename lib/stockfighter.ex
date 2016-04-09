1434740b4d1f5f381f28bf270512f672ad0c8be0
Steven Leiva <leiva.steven@gmail.com>
2016-04-08 22:25:33 -0400

    Accept arguments from the command line.
    - Add boilterplate code to accept command line arguments
    - Add option to mix.exs to build a command line interface

--- lib/stockfighter.ex
+++ lib/stockfighter.ex
@@ -1,2 +1,29 @@
 defmodule Stockfighter do
+  def main(args) do
+    args |> parse_args |> process
+  end
+
+  def fulfill_block_trade(_info) do
+  end
+
+  defp parse_args(args) do
+    case OptionParser.parse(args, switches: [account: :string, venue: :string, stock: :string, qty: :integer]) do
+     {options, _, []} ->
+       options
+
+     {_, _, unknown} ->
+       IO.puts "Unknown options passed"
+       Enum.each(unknown, fn ({key, value}) ->
+         IO.puts "#{key}: #{value}"
+       end)
+     end
+  end
+
+  def process([]) do
+    IO.puts "No options passed in!"
+  end
+
+  def process(args) do
+    fulfill_block_trade(args)
+  end
 end
--- mix.exs
+++ mix.exs
@@ -5,28 +5,17 @@ defmodule Stockfighter.Mixfile do
     [app: :stockfighter,
      version: "0.0.1",
      elixir: "~> 1.2",
+     escript: [main_module: Stockfighter],
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :httpoison]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:httpoison, "~> 0.8.0"}]
   end
 end
