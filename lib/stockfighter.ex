82bd49c378f601e9b7ef9d0a20bd4cabb01b1e9c
Steven Leiva <leiva.steven@gmail.com>
2016-04-09 00:13:08 -0400

    Add stockfighter executable to ignore list.
    - This can always be recreated from source by running mix escript.build.

--- lib/stockfighter.ex
+++ lib/stockfighter.ex
@@ -3,11 +3,41 @@ defmodule Stockfighter do
     args |> parse_args |> process
   end
 
-  def fulfill_block_trade(_info) do
+  def api_base_url do
+    "https://api.stockfighter.io/ob/api"
+  end
+
+  def api_key do
+    System.get_env("STOCKFIGHTER_API_KEY")
+  end
+
+  def calculate_midpoint(venue, stock) do
+    case HTTPoison.get("#{api_base_url}/venues/#{venue}/stocks/#{stock}") do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        IO.puts body
+
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        IO.puts "Not found :("
+
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect reason
+    end
+  end
+
+  def current_market_midpoint_price(venue, stock) do
+    calculate_midpoint(venue, stock)
+  end
+
+  def fulfill_block_trade(info) do
+    current_market_midpoint_price(Keyword.get(info, :venue), Keyword.get(info, :stock))
+  end
+
+  def headers do
+    [{"X-Starfighter-Authorization", api_key}]
   end
 
   defp parse_args(args) do
-    case OptionParser.parse(args, switches: [account: :string, venue: :string, stock: :string, qty: :integer]) do
+    case OptionParser.parse(args, switches: [account: :string, venue: :string, stock: :string]) do
      {options, _, []} ->
        options
 
